5055b655ad1b1775c2cf1e2915f4dc76bb916fe7
Ashwini3131 <93239185+Ashwini3131@users.noreply.github.com>
2023-08-09 00:03:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // your code here
+      document.getElementById("myForm").addEventListener("submit", function(event) {
+            event.preventDefault(); // Prevents the form from actually submitting
+            
+            // Get values from the input fields
+            const name = document.getElementById("name").value;
+            const year = document.getElementById("year").value;
+            let url = "https://localhost:8080/";
+            
+             
+             // Append query parameters based on input values
+            if (name && year) {
+                url += `?name=${encodeURIComponent(name)}&year=${year}`;
+            } else if (name) {
+                url += `?name=${encodeURIComponent(name)}`;
+            } else if (year) {
+                url += `?year=${year}`;
+            }
+            // Update the URL
+            document.getElementById("url").textContent = url;
+        });
